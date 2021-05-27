123af54f94c0f5bdf3f75567244a58c868264801
Flloki5 <rozli@op.pl>
2021-05-27 16:26:27 +0200

    Overloading greet method to handle multiple parameters

--- src/main/java/com/flloki/Introduce.java
+++ src/main/java/com/flloki/Introduce.java
@@ -3,13 +3,19 @@ package com.flloki;
 public class Introduce {
 
     public static String greet(String name){
+
         if(name == null) {
             return "Hello, my friend.";
         }else if(stringIsUpperCase(name)){
-            return "HELLO, " + name + ".";
+            return "HELLO, " + name + "!";
         }else {
             return "Hello, " + name + ".";
         }
+    }
+
+    public static String greet(String... names){
+
+        return "Hello, " + String.join(" and ", names) + ".";
     }
 
     public static boolean stringIsUpperCase(String name){
--- src/test/java/com/flloki/IntroduceTest.java
+++ src/test/java/com/flloki/IntroduceTest.java
@@ -13,13 +13,18 @@ public class IntroduceTest {
 
     @Test
     public void should_ReturnDefaultGreet_When_NameIsNull(){
-        assertEquals("Hello, my friend.", Introduce.greet(null));
+        assertEquals("Hello, my friend.", Introduce.greet((String) null));
     }
 
     @Test
     public void should_ReturnUppercase_When_NameIsUppercase(){
-        assertEquals("HELLO, BOB.", Introduce.greet("BOB"));
+        assertEquals("HELLO, BOB!", Introduce.greet("BOB"));
         assertEquals("Hello, BOb.", Introduce.greet("BOb"));
+    }
+
+    @Test
+    public void should_ReturnGreet_When_InputIsArrayOfNames(){
+        assertEquals("Hello, Bob and Marley.", Introduce.greet("Bob", "Marley"));
     }
 
 }
