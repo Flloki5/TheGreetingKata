651ce284cc677eae8a40b5b4a28b0a334e113eca
Flloki5 <rozli@op.pl>
2021-05-27 12:37:53 +0200

    First test created

---
+++ src/test/java/com/flloki/IntroduceTest.java
@@ -0,0 +1,15 @@
+package com.flloki;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class IntroduceTest {
+
+    @Test
+    public void should_ReturnGreet_ForGivenName(){
+        Introduce introduce = new Introduce();
+        assertEquals("Hello, Bob.", introduce.greet(name));
+    }
+
+}
